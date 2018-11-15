2eb2778001f80cd85875eaa7011971468cd327bf
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-11-15 20:52:54 +0100

    moves results to results/ folder

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,22 @@
 
 USER=binetruy
 HOST=c133-01
+RESULTS_FOLDER=results
 MODEL_NAME=$1
 
 # compile
 sbt package
 
 # upload
-ssh $USER@$HOST "mkdir -p kaggle/$MODEL_NAME"
-scp target/scala-2.11/simple-project_2.11-1.0.jar $USER@$HOST:~/kaggle/$MODEL_NAME
+ssh $USER@$HOST "mkdir -p kaggle/$RESULTS_FOLDER"
+ssh $USER@$HOST "mkdir -p kaggle/$RESULTS_FOLDER/$MODEL_NAME"
+scp target/scala-2.11/simple-project_2.11-1.0.jar $USER@$HOST:~/kaggle/$RESULTS_FOLDER/$MODEL_NAME
 
 # exec
-ssh $USER@$HOST "cd kaggle/$MODEL_NAME && ./../spark/bin/spark-submit simple-project_2.11-1.0.jar"
+ssh $USER@$HOST "cd kaggle/$RESULTS_FOLDER/$MODEL_NAME && ./../../spark/bin/spark-submit simple-project_2.11-1.0.jar"
 
 # repatriate result
-mkdir -p $MODEL_NAME
-scp $USER@$HOST:~/kaggle/$MODEL_NAME/results/*.csv $MODEL_NAME/results.csv
-scp $USER@$HOST:~/kaggle/$MODEL_NAME/f1-score $MODEL_NAME/f1-score
+mkdir -p $RESULTS_FOLDER
+mkdir -p $RESULTS_FOLDER/$MODEL_NAME
+scp $USER@$HOST:~/kaggle/$RESULTS_FOLDER/$MODEL_NAME/results/*.csv $RESULTS_FOLDER/$MODEL_NAME/results.csv
+scp $USER@$HOST:~/kaggle/$RESULTS_FOLDER/$MODEL_NAME/f1-score $RESULTS_FOLDER/$MODEL_NAME/f1-score
